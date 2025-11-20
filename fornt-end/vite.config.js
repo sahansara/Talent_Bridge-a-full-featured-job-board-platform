0e22b4a0d89d82b2de2053bba26e3387101dd551
sahansara <widanenadun@gmail.com>
2025-11-20 10:06:49 +0530

    update docker compose file

--- fornt-end/vite.config.js
+++ fornt-end/vite.config.js
@@ -1,27 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    host: '0.0.0.0', 
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+
     watch: {
-      usePolling: true, 
+      usePolling: true,
       interval: 1000,
     },
-    hmr: {
-      host: 'localhost',
-      port: 5173,
-    },
+
     
+    hmr: true,
+
     proxy: {
       '/api': {
         target: 'http://backend:3000',
         secure: false,
-        rewrite: (path) => path 
+        changeOrigin: true,
       }
     }
   },
